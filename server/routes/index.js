0c3595d791f4daadd3959e2424021f99e62d33ca
Paul Yun <pyun@wayfair.com>
2017-05-05 19:42:07 -0700

    Initial commit.

---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
+
+module.exports = (app) => {
+  /* Catch-all */
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Todos API!',
+  }));
+
+  /* Todos */
+  app.post('/api/todos', todosController.create);
+  app.get('/api/todos', todosController.list);
+
+  /* TodoItems */
+  app.post('/api/todos/:todoId/items', todoItemsController.create);
+  app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+};
